ff17c45efaf4f003bb3d4a2544ae253cf87f14df
nono <catjuicy@gmail.com>
2020-07-21 01:03:04 +0800

    确认是否唯一键错误时候，先检测err是否为nil

--- util.go
+++ util.go
@@ -11,6 +11,9 @@ import (
 //IsDuplicate 确认这个错误类型是不是唯一索引重复，如果是，返回true
 //主要目的是为了用户名或者手机号码，或者email的唯一性，很好用
 func IsDuplicate(err error) bool {
+	if err == nil {
+		return false
+	}
 	return strings.Contains(err.Error(), "E11000 duplicate key")
 }
 func s2i(s string) int64 {
